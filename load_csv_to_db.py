d43ca375171c69e96d4ee4cd74479a352022e3f1
Christopher Ayre <chris.nathan.ayre@gmail.com>
2022-02-27 18:25:45 +0800

    added script to load historical price CSV files from Fuelwatch into DB

--- load_csv_to_db.py
+++ load_csv_to_db.py
@@ -1,16 +1,101 @@
+#!/usr/bin/env python3
 import psycopg2
+from datetime import datetime
+import requests
 
-#Create table
-conn = psycopg2.connect("dbname=fuel" "user=postgres")
-cur = conn.cursor()
-cur.execute("CREATE TABLE PRICE (DATE DATE, LOCATION_NAME VARCHAR(45), FUEL_TYPE VARCHAR(15), PRICE DECIMAL(3,2), ADDRESS VARCHAR(40), SUBURB VARCHAR(30), POSTCODE INT, SITE_FEATURES VARCHAR(80), PRIMARY KEY(DATE, LOCATION_NAME, FUEL_TYPE)")
+CSV_LOCATION = "csv/"
+CSV_DOWNLOAD_NAME = "download"
 
-#Insert file data into table
+# Print iterations progress, From: https://stackoverflow.com/a/34325723
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+    # Print New Line on Complete
+    if iteration == total: 
+        print()
 
-#Test to see if data is in DB
 
-#Save changes to DB
-conn.commit()
-#Clean up
-cur.close()
-conn.close()
+def download(save_location):
+    #Needs zfill to add leading zeros if required.
+    month = str(datetime.now().month).zfill(2)
+    year = datetime.now().year
+    url = "https://warsydprdstafuelwatch.blob.core.windows.net/historical-reports/FuelWatchRetail-{0}-{1}.csv".format(month,year)
+    print("Downloading %s" % (url,))
+    r = requests.get(url, allow_redirects=True)
+    open(CSV_LOCATION + save_location + ".csv", 'wb').write(r.content)
+    print(url)
+
+
+def create_table():
+    try:
+        #Create table
+        conn = psycopg2.connect("dbname=fuel user=postgres")
+        cur = conn.cursor()
+        cur.execute("CREATE TABLE PRICE (DATE DATE, LOCATION_NAME VARCHAR(60), FUEL_TYPE VARCHAR(15), PRICE DECIMAL(5,2), ADDRESS VARCHAR(40), SUBURB VARCHAR(30), POSTCODE INT, PRIMARY KEY(DATE, LOCATION_NAME, FUEL_TYPE));")
+
+    except(psycopg2.errors.DuplicateTable):
+        print("Price table found.")
+
+    #Save changes to DB
+    conn.commit()
+    #Clean up
+    cur.close()
+    conn.close()
+
+
+def insert(file_name):
+    file_path = CSV_LOCATION + file_name + ".csv"
+    already_exist = 0
+
+    #Insert file data into table
+    print("Opening: %s" % (file_path,))
+    with open(file_path) as csv:
+        #Remove the first line:
+        string = csv.readline()
+        lines = []
+        for string in csv:
+            lines.append(string.split(","))
+        #PUBLISH_DATE,TRADING_NAME,BRAND_DESCRIPTION,PRODUCT_DESCRIPTION,PRODUCT_PRICE,ADDRESS,LOCATION,POSTCODE,AREA_DESCRIPTION,REGION_DESCRIPTION
+        #DB wants: date | location_name | fuel_type | price | address | suburb | postcode 
+        
+    sql = "INSERT INTO PRICE (DATE,LOCATION_NAME,FUEL_TYPE,PRICE,ADDRESS,SUBURB,POSTCODE) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+    print("Adding to DB")
+
+    length = len(lines)
+    for iteration, line in enumerate(lines):
+        printProgressBar(iteration,length)
+                #date,location, fueltype,price, address, suburb,postcode
+        data = (line[0],line[1],line[3],line[4],line[5],line[6], line[7])
+        try:
+            conn = psycopg2.connect("dbname=fuel user=postgres")
+            cur = conn.cursor()
+            cur.execute(sql, data)
+            #Save changes to DB
+            conn.commit()
+
+        except psycopg2.errors.UniqueViolation:
+            #print("Key: [%s,%s,%s] not added, already in DB" % (line[0],line[1],line[2]))
+            already_exist += 1
+    
+    print("%s/%s Already in DB" % (already_exist, len(lines)))
+    #Clean up
+    cur.close()
+    conn.close()
+
+create_table()
+download(CSV_DOWNLOAD_NAME)
+insert(CSV_DOWNLOAD_NAME)
--- main.py
+++ main.py
@@ -1,12 +1,52 @@
 #!/usr/bin/env python3
+import psycopg2
+from datetime import date, timedelta
 
-import fuelwatch.fuelwatch as fw
+# import fuelwatch.fuelwatch as fw
 
-opts3 = {'Product': 1, 'Suburb': "Queens Park"}
-url3 = fw.generate_url(opts3)
-data3 = fw.getdata(url3)
-results3 = fw.parse(data3)
+# opts3 = {'Product': 1, 'Suburb': "Queens Park"}
+# url3 = fw.generate_url(opts3)
+# data3 = fw.getdata(url3)
+# results3 = fw.parse(data3)
+# print("url3: ", url3)
+# print(results3)
 
-print("url3: ", url3)
+def average(data):
+    length = len(data)
+    average = 0.0
 
-print(results3)
+    for line in data:
+        average += float(line[3])
+
+    average = average/length
+
+    return average
+
+def ten_onehundred(postcode,fuel_type):
+    conn = psycopg2.connect("dbname=fuel user=postgres")
+    cur = conn.cursor()
+    #100 days ago was:
+    today = date.today()
+    delta = timedelta(days=100)
+    hundred_days_ago = today - delta
+    print(hundred_days_ago)
+    print(today)
+
+    sql = "SELECT * FROM PRICE WHERE POSTCODE=%s AND FUEL_TYPE=%s AND DATE BETWEEN %s AND %s ORDER BY DATE DESC"
+    data = (postcode,fuel_type, hundred_days_ago, today)
+    cur.execute(sql, data)
+    #data contains all prices from the required postcode and fuel type from the last 100days
+    data = cur.fetchall()
+    #Clean up
+    cur.close()
+    conn.close()
+
+    #Need to get average for ten days and 100 days then compare the two
+    
+    hundred_days_average = average(data)
+
+    print(hundred_days_average)
+
+
+
+ten_onehundred(6107,"ULP")
